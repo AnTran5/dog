42bba4bebbbfae7c7ca8d0dc83c7bbe74b1d49f6
antran <antran.hashmicro@gmail.com>
2019-04-24 10:51:22 +0800

    say Hi method for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,16 @@
-function Dog(){
-    
+var chalk =require('chalk')
+function Dog(name){
+    this.name=name;
+    this.stomach=[];
 }
 
-Dog.prototype{
-    this.stomach=this
+Dog.prototype.eat= function(cat){
+    this.stomach.push(cat)
 }
+
+Dog.prototype.sayHi=function(){
+    console.log(chalk.blue('Hello')+' Iam a dog. My name is' + chalk.red(this.name))
+
+}
+
+module.exports=Dog
